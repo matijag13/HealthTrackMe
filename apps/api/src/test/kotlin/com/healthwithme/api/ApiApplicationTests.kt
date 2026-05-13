353aa308581103adfebe3c25109533c3840db43c
Matija <matija.gusel@gmail.com>
2026-05-13 18:21:05 +0200

    support files

---
+++ apps/api/src/test/kotlin/com/healthwithme/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.healthwithme.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
